2cf9f1c5f586bd44b7b3a2762f85265b49ae65c2
Jana <janainecampos16@gmail.com>
2026-06-16 09:54:55 -0300

    Adicionar configurações do app

---
+++ conversor-pdf-excel-estrutural/assets/js/config/settings.js
@@ -0,0 +1,15 @@
+export const DEFAULT_SETTINGS = Object.freeze({
+  pageSpec: '',
+  mode: 'structural',
+  rowTolerance: 0.62,
+  columnTolerance: 9,
+  gapFactor: 2.3,
+  sheetPerPage: true,
+  mergeTitles: true,
+  maxPreviewRows: 22,
+  maxPreviewPages: 6,
+});
+
+export const PDFJS_WORKER = 'assets/js/vendor/pdf.worker.min.js';
+
+export const APP_NAME = 'Conversor PDF Excel Estrutural';
