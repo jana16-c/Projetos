e1b1388ef0d618d80bd3e7e05e285e07886c6a14
Jana <janainecampos16@gmail.com>
2026-06-16 10:34:28 -0300

    Adicionar testes automatizados da seleção de páginas

--- conversor-pdf-excel-estrutural/tests/pages.test.mjs
+++ conversor-pdf-excel-estrutural/tests/pages.test.mjs
@@ -1,10 +1,30 @@
 import assert from 'node:assert/strict';
 import { parsePageSpec } from '../assets/js/utils/pages.js';
+import {
+  buildDefaultPageSpec,
+  shouldApplyDefaultPageSpec,
+  selectionSummary,
+} from '../assets/js/utils/pageSelection.js';
 
 assert.deepEqual(parsePageSpec('', 3), [1, 2, 3]);
 assert.deepEqual(parsePageSpec('1-3, 5, 3', 6), [1, 2, 3, 5]);
 assert.deepEqual(parsePageSpec('4-2', 5), [2, 3, 4]);
+assert.deepEqual(parsePageSpec('2, 4-5', 10), [2, 4, 5]);
 assert.throws(() => parsePageSpec('0', 5), /fora do intervalo/);
 assert.throws(() => parsePageSpec('abc', 5), /Trecho inválido/);
+
+assert.equal(buildDefaultPageSpec(10), '1-10');
+assert.equal(
+  shouldApplyDefaultPageSpec({ currentValue: '2, 4-5', lastAutoValue: '1-10', userEdited: true }),
+  false,
+);
+assert.equal(
+  shouldApplyDefaultPageSpec({ currentValue: '', lastAutoValue: '1-10', userEdited: false }),
+  true,
+);
+assert.equal(
+  selectionSummary([2, 4, 5], 10),
+  '3 de 10 página(s) serão processadas: 2, 4, 5.',
+);
 
 console.log('pages.test.mjs OK');
